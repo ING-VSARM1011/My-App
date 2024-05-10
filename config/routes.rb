879832bd913e0074bedc98dfa9171194d880c766
LAPTOP-VUQ1J6NP\Victor_Moreno <victorsahir1011@gmail.com>
2024-05-10 02:45:45 -0500

    Se agrega el primer conjunto de scaffolds (usar el patrón de diseño asociado a una abstración de la realidad).

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :books
   get 'hola', to: 'welcome#hello'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_10_064433) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_10_073533) do
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "description"
+    t.string "isbn"
+    t.integer "pages_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "breed"
